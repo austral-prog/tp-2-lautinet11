5d20c57abcfb08ecbbd9564a9c6f1e0e9e9a0c0e
lautaro Cusinato <76267353+lautinet11@users.noreply.github.com>
2025-03-29 13:09:59 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,24 @@
-def change():
-    expense = 23.75
-    money = 100
+#Variables
+Gasto = 23.75
+Ingreso = 100
+Vueltop = 0
+VUeltoc = 0
+
+print("Ingresar gasto:")
+print(Gasto) # Deveria ser un input ( Pero sirve para demostrar asi)
+print("Dinero recibido")
+print(Ingreso) # Deveria ser un input (Sirve para chequear nomas asi)
+print("")
+print("Vuelto")
+print("")
+print("Pesos:")
+Ingreso = float(Ingreso)
+Gasto = float(Gasto)
+a = Ingreso - Gasto
+a = int(a)
+b = (Ingreso - Gasto)
+print(a)
+print("Centavos:")
+c = (b - a) * 100
+c = int(c)
+print(c)
