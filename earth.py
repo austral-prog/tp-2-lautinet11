36bfc84c54e10c8b382976816ed72987a20fe7cd
lautaro Cusinato <76267353+lautinet11@users.noreply.github.com>
2025-03-29 13:10:40 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,11 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
+    c = x < y
+    d = x > y
+    c = str(c)
+    d = str(d)
+
+    print("The result of "+x+" comes first in the dictionary than "+y+" is " + c +".")
+    print("The result of "+y+" comes first in the dictionary than "+x+" is " + d + ".")
+earth()
